fab4fec9035a4aaf58dd024eb7b9e71dcc0eebab
Szymon Strugala <szymonstrugala23@gmail.com>
2019-09-12 23:48:48 +0200

    Add input handler

--- src/application.cpp
+++ src/application.cpp
@@ -1,4 +1,5 @@
 #include "application.hpp"
+#include "inputHandler.hpp"
 
 const int mWidthOfTheGrid = 20;
 const int mHeightOfTheGrid = 10;
@@ -40,7 +41,9 @@ void Application::draw()
 }
 
 void Application::processEvents()
+
 {
+	InputHandler::clear();
 	sf::Event event;
 	while (mWindow.pollEvent(event))
 	{
@@ -49,9 +52,34 @@ void Application::processEvents()
 		case sf::Event::Closed:
 			mExit = true;
 			break;
+
 		case sf::Event::Resized:
+		{
 			sf::FloatRect fixedView(0, 0, event.size.width, event.size.height);
 			mWindow.setView(sf::View(fixedView));
+			break;
+		}
+
+		case sf::Event::MouseMoved:
+		{
+			sf::Vector2i mouseViewPosition = sf::Mouse::getPosition(mWindow);
+			sf::Vector2f mouseWorldPosition = mWindow.mapPixelToCoords(mouseViewPosition);
+			InputHandler::setMouseViewPosition(mouseViewPosition);
+			InputHandler::setMouseWorldPosition(mouseWorldPosition);
+			break;
+		}
+
+		case sf::Event::MouseButtonPressed:
+			InputHandler::setLastMouseViewPressPosition();
+			InputHandler::setLastMouseWindowPressPosition();
+			break;
+
+		case sf::Event::KeyPressed:
+		{
+			InputHandler::setLastPressedKey(event.key.code);
+			break;
+		}
+
 		}
 	}
 }
