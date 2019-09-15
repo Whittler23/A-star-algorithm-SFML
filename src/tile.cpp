39c786c073ccb24be7014a948557aec5ba122afc
Szymon Strugala <szymonstrugala23@gmail.com>
2019-09-15 22:38:10 +0200

    Add TileType

--- src/tile.cpp
+++ src/tile.cpp
@@ -3,14 +3,39 @@
 Tile::Tile(const sf::Vector2f& position, const int size)
 	:mTileSize(size)
 	,mTileImage(sf::Vector2f(mTileSize, mTileSize))
+	,mTileType(TileType::None)
 {
 	mTileImage.setPosition(position);
 	mTileImage.setOutlineColor(sf::Color::Black);
-	mTileImage.setOutlineThickness(mTileSize/10.f);
-	mTileImage.setFillColor(sf::Color::Red);
+	mTileImage.setOutlineThickness(mTileSize/25.f);
 }
 
 void Tile::draw(sf::RenderTarget& renderTarget, sf::RenderStates renderStates) const
 {
 	renderTarget.draw(mTileImage);
+}
+
+void Tile::setType(TileType tileType)
+{
+	mTileType = tileType;
+	switch (mTileType)
+	{
+	case TileType::StartingTile:
+		mTileImage.setFillColor(sf::Color::Green);
+		break;
+	case TileType::TargetedTile:
+		mTileImage.setFillColor(sf::Color::Red);
+		break;
+	case TileType::PathTile:
+		mTileImage.setFillColor(sf::Color::Blue);
+		break;
+	case TileType::HoverTile:
+		mTileImage.setFillColor(sf::Color::Blue);
+		break;
+	case TileType::None:
+		mTileImage.setFillColor(sf::Color::White);
+		break;
+	default:
+		break;
+	}
 }
--- src/tile.hpp
+++ src/tile.hpp
@@ -2,14 +2,21 @@
 
 #include <SFML/Graphics.hpp>
 
+enum class TileType
+{
+	StartingTile, TargetedTile, PathTile, HoverTile, None
+};
+
 class Tile : public sf::Drawable, public sf::Transformable
 {
 public:
 	Tile(const sf::Vector2f& tilePosition, const int tileSize);
 
 	void draw(sf::RenderTarget& renderTarget, sf::RenderStates renderStates) const override;
+	void setType(TileType tileType);
 
 private:
 	int mTileSize;
 	sf::RectangleShape mTileImage;
+	TileType mTileType;
 };
